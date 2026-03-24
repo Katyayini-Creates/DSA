f74bcdb95bf4057d9d22ee9ef59392b7becf1cf6
Katyayini-Creates <katyayinis.04@gmail.com>
2026-03-24 22:09:28 +0530

    Demonstrated basic use of conditionals.

---
+++ Basics/Conditionals.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Conditionals {
+    public static void main(String[] args) {
+//        int salary = 5_000;
+//        if(salary > 10_000)
+//        {
+//            salary+=2000;
+//        } else {
+//            salary+=5000;
+//        }
+//        System.out.println("Your salary with bonus= "+salary);
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("enter your salary: ");
+//        int salary = input.nextInt();
+//
+//        if(salary<10_000){
+//            salary+=5000;
+//        } else if (salary <30_000 && salary > 10_000){
+//            salary+=3000;
+//        } else {
+//            salary+=2000;
+//        }
+//        System.out.println("your salary with bonus= " + salary);
+    }
+}
