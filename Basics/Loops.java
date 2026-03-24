de831ca59236878b4d75b17e4975c728a650f492
Katyayini-Creates <katyayinis.04@gmail.com>
2026-03-24 22:10:28 +0530

    Demonstarted the basic usage of loops.

---
+++ Basics/Loops.java
@@ -0,0 +1,20 @@
+public class Loops {
+    public static void main(String[] args) {
+        // print number from 1 to 5
+//        for (int num = 1; num < 6; num++) {
+//            System.out.println(num);
+//        }
+
+        //print hello 100 times
+//        for (int i = 1; i <=100 ; i++) {
+//            System.out.println("Hello");
+//        }
+
+        // print number from 1 to 5
+        int num = 1;
+        while(num<6){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
