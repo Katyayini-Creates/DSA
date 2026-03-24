c3439e06468fd982fce8c339c2ab76ba268c5d37
Katyayini-Creates <katyayinis.04@gmail.com>
2026-03-24 22:12:26 +0530

    Prints the largest number among the three numbers.

---
+++ Basics/LargestOfThree.java
@@ -0,0 +1,18 @@
+public class LargestOfThree {
+    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 50;
+//        int c = 30;
+//        int greatest = a;
+//
+//        if(b > a && b > c){
+//            greatest = b;
+//        } else if(c > a && c > b){
+//            greatest = c;
+//        }
+//        System.out.println("greatest number out of given numbers= " + greatest);
+
+        int max = Math.max(89 , Math.max(26 , 76));
+        System.out.println(max);
+    }
+}
