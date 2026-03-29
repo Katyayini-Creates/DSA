c64b70abb22175c80a1f1f6c4670f4493539a92b
Katyayini-Creates <katyayinis.04@gmail.com>
2026-03-29 22:26:48 +0530

    Showed the difference of pass by value in primitives and non-primitives.

--- Basics/PassingExample.java
+++ Basics/PassingExample.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class PassingExample {
+    public static void main(String[] args) {
+        String name = "Katyayini";
+        change(name);
+        System.out.println(name);
+
+        int[] arr = {1, 2, 3, 4};
+        change2(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void change(String naam) {  //copy of the value stored in reference variable (name) is stored in 'naam'
+        naam = "Singh";
+    }
+
+    static void change2(int[] nums) {  //non-primitives are pass by value of reference
+        nums[1] = 99;
+    }
 }
