ff3ae221b9a3164d3c1c185887ced2fceefdee2c
Katyayini-Creates <katyayinis.04@gmail.com>
2026-03-31 19:57:12 +0530

    Showed method, block and loop scope.

--- Basics/Scope.java
+++ Basics/Scope.java
@@ -2,15 +2,15 @@ public class Scope {
     public static void main(String[] args) {
         int a = 10;
         {
-            int a = 100;  //same variable can't be reinitialized inside another block. (block scope)
-            a = 20; //value can be modified. (block scope)
-
-            int b = 90;
+//            int a = 100;  //same variable can't be reinitialized inside another block. (block scope)
+//            a = 20; //value can be modified. (block scope)
+//
+//            int b = 90;
         }
-        b = 20;                  /*variable initialized inside an internal block can't be used outside. (block scope)*/
-        System.out.println(b);
-
-        System.out.println(num);  //variable of another method can't be directly used inside another method (method scope).
+//        b = 20;                  /*variable initialized inside an internal block can't be used outside. (block scope)*/
+//        System.out.println(b);
+//
+//        System.out.println(num);  //variable of another method can't be directly used inside another method (method scope).
     }
 
     static void change(int c) {
@@ -19,6 +19,6 @@ public class Scope {
         for(int i = 0; i <= 5; i++) {
             System.out.println(i);
         }
-        System.out.println(i);        //variable of/inside loop can't be used outside (loop scope).
+        // System.out.println(i);        //variable of/inside loop can't be used outside (loop scope).
     }
 }
