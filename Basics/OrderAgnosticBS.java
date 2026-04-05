d7897b2c697766c81762326d979dcd93795eed63
Katyayini-Creates <katyayinis.04@gmail.com>
2026-04-05 13:03:04 +0530

    Demonstarted order-agnostic binary search (handling arrays when we don't know that in which order they are sorted).

---
+++ Basics/OrderAgnosticBS.java
@@ -0,0 +1,37 @@
+public class OrderAgnosticBS {
+    public static void main(String[] args) {
+        int[] arr = {99, 78, 67, 45, 23, 12};
+        int target = 100;
+
+        int result = binSearch(arr, target);
+        if(result != -1) {
+            System.out.println("The target element is found at index " + result);
+        } else {
+            System.out.println("The target element is not found.");
+        }
+
+    }
+    static int binSearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        boolean asc = arr[start] < arr[end];
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if(asc) {
+                if(target < arr[mid]) {
+                    end = mid - 1;
+                } else if (target > arr[mid]) {
+                    start = mid + 1;
+                } else return mid;
+            } else {
+                if(target < arr[mid]) {
+                    start = mid + 1;
+                } else if (target > arr[mid]) {
+                    end = mid - 1;
+                } else return mid;
+            }
+        }
+        return -1;
+    }
+}
